69da0d381bd85bc3b2c288f511f6e16ae073d32e
Michellangello27 <cor24023@byui.edu>
2025-03-26 14:06:24 -0500

    ventana de solapamiento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,5 +10,36 @@ import { loadStays } from './utils.js';
 
 const staysContainer = document.querySelector('#stays-contenedor');
 
+
+import {
+  loadStays,
+  filterByCity,
+  filterByGuests,
+  getUniqueCities,
+} from './utilsF.js';
+
+
+const deskLocationInput = document.querySelector('#deskLocationInput');
+const mobLocationInput = document.querySelector('#mobLocationInput');
+const locationSug = document.querySelector('#locationSug');
+const mobLocationSug = document.querySelector('#mobLocationSug');
+
+const guestsButton = document.querySelector('#guests-button');
+const mobileGuestInput = document.querySelector('#mobGuestInput');
+const mobileSearchBtn = document.querySelector('#mobSearchBtn');
+
+const ventana = document.querySelector('#ventana-busqueda');
+const closeVentana = document.querySelector('#close-ventana');
+
+
+const adultsCount = document.querySelector('#adults-count');
+const childrenCount = document.querySelector('#children-count');
+const guestButtons = document.querySelectorAll('.guest-btn');
+
+let adults = 0;
+let children = 0;
+
 loadStays(stays, staysContainer);
+
+
 
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,42 +1,56 @@
 /**
- * Módulo de funciones utilitarias.
- * Este archivo contiene funciones auxiliares que serán utilizadas y llamadas
- * desde el archivo principal para realizar varias operaciones.
+ * Módulo de funciones utilitarias para Windbnb.
+ * Aquí se gestionan funciones de renderizado, filtrado y ayuda general.
  */
 
 export function loadStays(array, elementHTML) {
-    elementHTML.innerHTML = "";
-  
-    array.forEach(function(stay) {
-      const template = stayTemplate(stay);
-      elementHTML.innerHTML += template;
-    });
-  }
-  
-  export function stayTemplate(stay) {
-    return `
-      <div class="rounded-xl overflow-hidden">
-        <img src="${stay.photo}" alt="${stay.title}" class="w-full h-60 object-cover rounded-xl" />
-        <p class="flex items-center gap-2 text-sm text-gray-500 mt-2">
-          //*${stay.superHost}  '<span class="text-xs font-bold border px-2 py-0.5 rounded-md">SUPERHOST</span>' : ''*/
-          ${stay.type}
-        </p>
-        <div class="flex justify-between items-center mt-1">
-          <p class="font-semibold">${stay.title}</p>
-          <p class="text-sm text-red-500 font-medium">★ ${stay.rating}</p>
-        </div>
-      </div>
-    `;
-  }
-  
- /*
-  export function filterByCity(city, stays) {
-    return stays.filter((stay) => stay.city.toLowerCase() === city.toLowerCase());
-  }
-  */
+  elementHTML.innerHTML = "";
 
-  /*
-  export function filterByGuests(guests, stays) {
-    return stays.filter((stay) => stay.maxGuests >= guests);
+  array.forEach(function (stay) {
+    const template = stayTemplate(stay);
+    elementHTML.innerHTML += template;
+  });
+}
+
+export function stayTemplate(stay) {
+    if(stay.superHost){
+      return `
+        <div class="rounded-xl overflow-hidden shadow-sm border">
+          <img src="${stay.photo}" alt="${stay.title}" class="w-full h-60 object-cover rounded-xl" />
+          <p class="flex items-center gap-2 text-sm text-gray-500 mt-2">
+            <span class="text-xs font-bold border px-2 py-0.5 rounded-md">SUPERHOST</span>'
+            ${stay.type}.${stay.beds}
+          </p>
+          <div class="flex justify-between items-center mt-1">
+            <p class="font-semibold text-sm">${stay.title}</p>
+            <p class="text-sm text-[#eb5757] font-medium">★ ${stay.rating}</p>
+          </div>
+        </div>
+      `;
+    }
+    return `
+        <div class="rounded-xl overflow-hidden shadow-sm border">
+          <img src="${stay.photo}" alt="${stay.title}" class="w-full h-60 object-cover rounded-xl" />
+          <p class="flex items-center gap-2 text-sm text-gray-500 mt-2">
+            ${stay.superHost}
+            <span class="text-xs font-bold border px-2 py-0.5 rounded-md">SUPERHOST</span>'
+            ${stay.type}${stay.beds}
+          </p>
+          <div class="flex justify-between items-center mt-1">
+            <p class="font-semibold text-sm">${stay.title}</p>
+            <p class="text-sm text-[#eb5757] font-medium">★ ${stay.rating}</p>
+          </div>
+        </div>
+      `;
   }
-*/
+
+
+export function filterByCity(city, stays) {
+  return stays.filter((stay) => stay.city.toLowerCase() === city.toLowerCase());
+}
+
+export function filterByGuests(guests, stays) {
+  return stays.filter((stay) => stay.maxGuests >= guests);
+}
+
+
