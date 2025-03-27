edc7c0fd38cbca519bae3c114cdc2a7a048aefdb
Michellangello27 <cor24023@byui.edu>
2025-03-27 15:01:33 -0500

    para revision funcion cerrar modal

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -41,12 +41,3 @@ import { loadStays, toggleModal, addCityFilter } from './utils.js';
     toggleModal();
   });
 
-  // Cerrar al hacer clic fuera del modal
-  window.addEventListener('click', (e) => {
-    const modal = document.getElementById('modal');
-    const content = document.getElementById('modal-content');
-    if (modal && content && !content.contains(e.target) && !modal.classList.contains('hidden')) {
-      console.log("Clic fuera del modal");
-      modal.classList.add('hidden');
-    }
-  });
